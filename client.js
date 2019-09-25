bed5ed3b09ba293e625ec4df342a0de605674824
Brandon <brandonjamiller@hotmail.com>
2019-09-25 23:16:26 +0000

    clients.js updated with constants entered, pair programming with Jacky

--- client.js
+++ client.js
@@ -1,25 +1,28 @@
+const { IP, PORT } = require('./constants');
+
 const net = require('net');
 
+
 const connect = function() {
-  const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
   });
-  conn.on('connect', (data) =>{
-    console.log('Successfully connected to game server')
+  conn.on('connect', () =>{
+    console.log('Successfully connected to game server');
     conn.write("Name: BJM");
 
-  })
+  });
 
   
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
   conn.on('data', (data) => {
-    console.log(data)  
-})
+    console.log(data);
+  });
 
 
   return conn;
-}
+};
 
 module.exports = { connect };
