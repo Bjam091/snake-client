2abb899bf872b7746339fd78d7ce691d1d714043
Brandon <brandonjamiller@hotmail.com>
2019-09-25 22:52:41 +0000

    play.js, with connection added, pair programming with Jacky

--- play.js
+++ play.js
@@ -7,6 +7,6 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
+let connection = connect();
 
-setupInput();
+setupInput(connection);
