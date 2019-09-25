c8d96c608e01c4386a0ca5f4b6576e614359f400
Brandon <brandonjamiller@hotmail.com>
2019-09-25 23:17:37 +0000

    input.js with eslint ran, pair programing with Jacky

--- input.js
+++ input.js
@@ -1,7 +1,7 @@
 let connection;
 
 const setupInput = function(conn) {
-  connection = conn
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -9,30 +9,23 @@ const setupInput = function(conn) {
   const handleUserInput = function(data) {
     if (data === '\u0003') {
       process.exit();
+    } else if (data === 'w') {
+      conn.write("Move: up");
+    } else if (data === 'a') {
+      conn.write("Move: left");
+    } else if (data === 's') {
+      conn.write("Move: down");
+    } else if (data === 'd') {
+      conn.write("Move: right");
+    } else if (data === 'r') {
+      conn.write("Say: I'm going to get you!");
+    } else if (data === 't') {
+      conn.write("Say: Snakes for nothing!");
+    } else if (data === 'f') {
+      conn.write("Say: Muhahah!");
     }
-    else if (data === 'w'){
-      conn.write("Move: up")
-    }
-    else if (data === 'a'){
-      conn.write("Move: left")
-    }
-    else if (data === 's'){
-      conn.write("Move: down")
-    }
-    else if (data === 'd'){
-      conn.write("Move: right")
-    }
-    else if (data === 'r'){
-      conn.write("Say: I'm going to get you!")
-    }
-    else if (data === 't'){
-      conn.write("Say: Snakes for nothing!")
-    }
-    else if (data === 'f'){
-      conn.write("Say: Muhahah!")
-    }
-  }
+  };
   stdin.on('data', handleUserInput);
-}
+};
 
-module.exports = { setupInput }
+module.exports = { setupInput };
