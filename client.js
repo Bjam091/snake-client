2ee633c1024a08d5b0ebeb927e64d9a00fdf526a
Brandon <brandonjamiller@hotmail.com>
2019-09-25 21:05:44 +0000

    client.js with connection message and name implementation, pair programming with Jacky

--- client.js
+++ client.js
@@ -1,15 +1,21 @@
 const net = require('net');
 
-const connect = function(data) {
+const connect = function() {
   const conn = net.createConnection({ 
     host: '10.0.2.15',
     port: 50541
   });
+  conn.on('connect', (data) =>{
+    console.log('Successfully connected to game server')
+    conn.write("Name: BJM")
+  })
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('data', (data) => {
-    console.log(data)
+    console.log(data)  
 })
+
+
 
   return conn;
 }
